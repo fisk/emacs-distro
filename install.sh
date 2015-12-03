e02fe69ef72d1f088a3ca4d875ca702fc876cce6
eosterlund <erik.osterlund@lnu.se>
2015-12-03 22:38:52 +0100

    New stuff

--- install.sh
+++ install.sh
@@ -4,9 +4,12 @@ FISK_EMACS_URL="https://github.com/fisk/emacs-distro.git"
 PRELUDE_URL="https://github.com/bbatsov/prelude.git"
 PRELUDE_INSTALL_DIR="$HOME/.emacs.d"
 
+# Install Prelude
 curl -L https://git.io/epre | sh
 
-cp "$HOME/.emacs.d/sample/prelude-modules.el" "$HOME/.emacs.d/"
+# Get default prelude-modules file
+cp "$PRELUDE_INSTALL_DIR/sample/prelude-modules.el" "$PRELUDE_INSTALL_DIR/"
 
+# Clone personal repo
 /usr/bin/env git clone $FISK_EMACS_URL "$PRELUDE_INSTALL_DIR/personal"
 
